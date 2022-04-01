0c90403edc2ba36439d329281165000aa44a2b7c
REVOLVERSNAKE24 <102831654+REVOLVERSNAKE24@users.noreply.github.com>
2022-04-01 14:53:06 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 st.title("My First App")
 click = st.button ("Inicio") 
- 
+st.write ("ES VERDAD O FALSO", click)
+
+if click== True:
+    st.image("images.png")
 #st.button ("Seccion 1")
 
 # Estas Lienas fueron para agregar una hoja de calculo y un mapa con datos represntados en puntos y una tabla
