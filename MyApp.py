83f9f41adb5f2afc0787f14b34dc31177a430ee4
REVOLVERSNAKE24 <102831654+REVOLVERSNAKE24@users.noreply.github.com>
2022-04-01 14:28:20 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 st.title("My First App")
 #st.button ("Inicio")  
 #st.button ("Seccion 1")
 
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+
+st.write(df)
+
+st.map(df)
